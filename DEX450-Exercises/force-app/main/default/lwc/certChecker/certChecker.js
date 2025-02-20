fd0aa1e0f9f336c9950c0d451a4ae580b312fe42
AReeves8 <areeves@skillstorm.com>
2025-02-20 11:10:56 -0600

    lesson 14 exercises

---
+++ DEX450-Exercises/force-app/main/default/lwc/certChecker/certChecker.js
@@ -0,0 +1,14 @@
+import { api, LightningElement } from 'lwc';
+
+export default class CertChecker extends LightningElement {
+
+
+    @api recordId;
+    techIds;
+
+
+    handleSelectedIds(event) {
+        this.techIds = event.detail.techIds;
+    }
+
+}
